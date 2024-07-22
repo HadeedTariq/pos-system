2411eacbddd5171c99db47c896d2f967f51cb61a
hadeedtariq <computeranalog351@gmail.com>
2024-07-22 08:37:00 +0100

    swagger: add swagger in backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,21 +4,29 @@ import { ConfigService } from '@nestjs/config';
 import { connectToDb } from './dbConnection/connectToDb';
 import { CustomExceptionFilter } from './exception.filter';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
   const configureService = app.get(ConfigService);
   const port = configureService.get<number>('PORT');
   const clientUrl = configureService.get<string>('CLIENT_URL');
-
   app.enableCors({
     origin: [clientUrl, 'http://192.168.10.7:5173'],
     credentials: true,
     exposedHeaders: ['Set-Cookie'],
   });
   app.use(cookieParser());
-  // app.use(passport.initialize());
   app.useGlobalFilters(new CustomExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('QuickSell API')
+    .setDescription('Quick Sell Api a pos system')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   await connectToDb();
 }
